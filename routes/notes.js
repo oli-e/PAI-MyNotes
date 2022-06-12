22d653fea2561f210332d185a666af1206e513ed
Olena Chorna <olena.chorna@aptiv.com>
2022-06-12 15:35:55 +0200

    editing added

--- routes/notes.js
+++ routes/notes.js
@@ -13,8 +13,6 @@ router.get('/', function(req, res, next) {
             
             let notes_query = `SELECT Title, Text, Tags FROM Notes WHERE "User ID"=${req.user_id}`;
             db.all(notes_query, (err, data) => {
-                console.log(data);
-                console.log(req.user_id);
                 res.render('notes_list', {user: user_login, data: data});
             });
         });
@@ -31,31 +29,94 @@ router.get('/create', (req, res) => {
 
 router.post('/create', (req, res) => {
 
-    var db = new sqlite3.Database('notes.db');
+    if (req.authorized) {
+        var db = new sqlite3.Database('notes.db');
 
-    let note_title = req.body.title;
-    let note_text = req.body.text;
-    let note_tags = req.body.tags;
+        let note_title = req.body.title;
+        let note_text = req.body.text;
+        let note_tags = req.body.tags;
 
-    let query =`INSERT INTO Notes(Title, Text, Tags, "User ID") VALUES("${note_title}", "${note_text}", "${note_tags}", "${req.user_id}")`;
-    console.log(query);
+        let query = `INSERT INTO Notes(Title, Text, Tags, "User ID") VALUES("${note_title}", "${note_text}", "${note_tags}", "${req.user_id}")`;
 
-    db.run(query);
+        db.run(query);
 
-    // res.send("New note saved.");
-    res.redirect('/notes');
+        res.redirect('/notes');
+    }else{
+        req.status = 403;
+        res.redirect('/users/login');
+    }
 });
 
 router.get('/delete:title', (req, res) => {
 
-    var db = new sqlite3.Database('notes.db');
+    if (req.authorized) {
+        var db = new sqlite3.Database('notes.db');
+        let query = `DELETE FROM Notes WHERE Title="${req.params.title}" AND "User ID"=${req.user_id};`;
+        db.run(query);
 
-    console.log(req.params.title, req.user_id);
-    let query = `DELETE FROM Notes WHERE Title="${req.params.title}" AND "User ID"=${req.user_id};`;
-    db.run(query);
-
-    console.log("DELETE");
-    res.redirect('/notes');
+        res.redirect('/notes');
+    } else {
+        req.status = 403;
+        res.redirect('/users/login');
+    }
 });
+
+router.get('/update:title', (req, res) => {
+    if (req.authorized) {
+
+        var db = new sqlite3.Database('notes.db');
+        let notes_query = `SELECT Title, Text, Tags FROM Notes WHERE Title="${req.params.title}" AND "User ID"=${req.user_id}`;
+        db.all(notes_query, (err, data) => {
+            if (data.length >= 1) {
+                res.render('update_note', { row: data[0] });
+            } else {
+                res.redirect('/notes')
+            }
+        });
+    } else {
+        req.status = 403;
+        res.redirect('/users/login');
+    }
+});
+
+router.post('/update:title', (req, res) => {
+    
+    if (req.authorized) {
+        var db = new sqlite3.Database('notes.db');
+        
+        let new_title = req.body.title;
+        let new_text = req.body.text;
+        let new_tag = req.body.tags;
+
+        let notes_query = `SELECT Title, Text, Tags FROM Notes WHERE Title="${req.params.title}" AND "User ID"=${req.user_id}`;
+        db.all(notes_query, (err, data) => {
+            if (data.length >= 1) {
+                console.log(data);
+                if (new_title.length == 0) {
+                    new_title = data[0].Title;
+                }
+                if (new_text.length == 0) {
+                    new_text = data[0].Text;
+                }
+        
+                if (new_tag.length == 0) {
+                    new_tag = data[0].Tags;
+                }
+        
+                let update_query = `UPDATE Notes SET Title = "${new_title}", Text = "${new_text}", Tags = "${new_tag}" WHERE "User ID"=${req.user_id} AND Title="${req.params.title}";`;
+                db.run(update_query);
+        
+                res.redirect('/notes');
+            }
+        });
+
+    } else { 
+        req.status = 403;
+        res.redirect('/users/login');
+    }
+});
+
+
+
 
 module.exports = router;
